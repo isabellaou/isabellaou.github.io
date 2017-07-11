14ed370111b0fb6f931fff05c96c2fceee9d2d67
Hanqi Ou <ouhanqi1998@gmail.com>
2017-07-11 13:48:31 -0400

    Smooth Scrolling Added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
-$(".collapse-bar-btn").on("click", function () {
-	$(".navbar_1").addClass("appear");
-});
-$(".collapse-btn").on("click", function () {
-	$(".navbar_1").removeClass("appear");
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (500) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 500, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
 });
